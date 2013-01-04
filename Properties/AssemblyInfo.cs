7f075b38e8b2cc5ccbc8504e66f6416c90cc607e
Vincent de Lagabbe <v.delagabbe@criteo.com>
2013-01-04 10:48:04 +0100

    update assembly info

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("MaxMind.GeoIP")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("MaxMind GeoIP C# API")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("MaxMind")]
 [assembly: AssemblyProduct("MaxMind.GeoIP")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2012")]
+[assembly: AssemblyCopyright("Copyright © MaxMind 2008")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
